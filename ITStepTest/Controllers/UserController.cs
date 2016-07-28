20d6bd0e9875d4c01aebc2d9434c9340eab536fa
Maks Bugrov <www.maks-ky@list.ru>
2016-07-28 09:50:50 +0300

    added edit user

--- ITStepTest/Controllers/UserController.cs
+++ ITStepTest/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using ITStepTest.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,7 +10,7 @@ namespace ITStepTest.Controllers
 {
     public class UserController : Controller
     {
-
+        private StoreDBEntities db = new StoreDBEntities();
         private UserService userService = new UserService();
         private MessageService messageService = new MessageService();
         //
@@ -27,6 +28,43 @@ namespace ITStepTest.Controllers
             return View(userService.GetAll());
         }
 
+        
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            User selectUser = db.Users.Find(id);
+            if (User.Identity.IsAuthenticated)
+            {
+                var userName = User.Identity.Name;
+                var user = userService.GetByName(userName);
+                ViewBag.User = user;
+                ViewBag.Messages = messageService.GetRecepientNotReadCount(user.Id);
+                ViewBag.Groups = db.Groups.ToList();
+            }
+            if (selectUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(selectUser);
+        }
+
+        //
+        // POST: /Group/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
         public ActionResult Activate(int id = 0)
         {
             userService.ActivateUserById(id, true);
@@ -37,6 +75,12 @@ namespace ITStepTest.Controllers
         {
             userService.ActivateUserById(id, false);
             return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
     }
